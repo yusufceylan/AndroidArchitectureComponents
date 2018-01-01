214c590deadd16ed40b3e2390254ffc526610496
yusufceylan <ysf.cyln@yandex.com>
2018-01-01 22:37:50 +0300

    Create UI resources

--- app/src/main/java/ysfcyln/com/acviewmodel/ListFragment.java
+++ app/src/main/java/ysfcyln/com/acviewmodel/ListFragment.java
@@ -1,10 +1,49 @@
 package ysfcyln.com.acviewmodel;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Yusuf on 01.01.2018.
  */
 
 public class ListFragment extends Fragment {
+
+    @BindView(R.id.recycler_view)
+    RecyclerView listView;
+
+    @BindView(R.id.tv_error)
+    TextView errorTextView;
+
+    @BindView(R.id.loading_view)
+    View loadingView;
+
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.screen_list, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (unbinder != null) {
+            unbinder.unbind();
+            unbinder = null;
+        }
+    }
 }
